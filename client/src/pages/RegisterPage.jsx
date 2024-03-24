303c22fa84cb87850703042241ab678b4581ff85
Pritam <pritammagdum24@gmail.com>
2024-03-24 17:34:49 +0530

    Added Functionality to Register or Create the User Account

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,45 @@
 import { Button, TextInput } from "flowbite-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log({ name, email, password });
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    axios.post("/register", { name, email, password });
+  };
+
   return (
     <div className="flex justify-center">
       <div className="min-w-96">
         <h1 className="text-center text-4xl font-medium">Register</h1>
-        <form className="flex flex-col gap-2 py-4">
-          <TextInput type="text" placeholder="Your Name" />
-          <TextInput type="email" placeholder="example@gmail.com" />
-          <TextInput type="password" placeholder="Your Password" />
-          <Button gradientDuoTone="pinkToOrange">Register</Button>
+        <form className="flex flex-col gap-2 py-4" onSubmit={registerUser}>
+          <TextInput
+            type="text"
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextInput
+            type="email"
+            placeholder="example@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextInput
+            type="password"
+            placeholder="Your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button gradientDuoTone="pinkToOrange" type="submit">
+            Register
+          </Button>
           <div className="text-sm font-semibold">
             Already registered?
             <Link
