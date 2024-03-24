cd649d58c17b9338ff6dba99b4ba92c7a73f87fe
Pritam <pritammagdum24@gmail.com>
2024-03-24 17:37:47 +0530

    Added .env to store MongoDB URL and Connected with MongoDB with the Help of Mongoose Package or Library

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const User = require("./models/User.js");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hey User it is working really");
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+app.get("/test", (req, res) => {
+  res.json("Hey User it is working really");
 });
+
 app.listen(8080);
