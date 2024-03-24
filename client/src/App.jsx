46eab6d4ac15c55208e1bea09ec7b39b46a89371
Pritam <pritammagdum24@gmail.com>
2024-03-24 15:55:12 +0530

    Added Routes of Login and Register Endpoint with the Help of React Router Dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import Header from "./components/Header";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <>
-      <div className="text-2xl bg-gray-500">
-        Hello Airbnb I will be there in soon
-      </div>
-    </>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
