c7e9181ea84ee21958ef0c10212269d656d88046
Pritam <pritammagdum24@gmail.com>
2024-03-24 18:17:27 +0530

    Added Endpoint or API to Create User Account

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 require("dotenv").config();
-// const User = require("./models/User.js");
+const User = require("./models/User.js");
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(
@@ -18,4 +21,19 @@ app.get("/test", (req, res) => {
   res.json("Hey User it is working really");
 });
 
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const newUser = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+
+    res.json(newUser);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
 app.listen(8080);
