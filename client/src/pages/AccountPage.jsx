2625a92fb781e52906b95e05cbaf2dc4eda6ea6d
Pritam <pritammagdum24@gmail.com>
2024-03-25 11:39:06 +0530

    Added Account Page with Profile Information

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,70 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../components/UserContext";
+import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { Button } from "flowbite-react";
+
+export default function AccountPage() {
+  const { ready, user, setUser } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(false);
+
+  if (!ready) {
+    return "Loading...";
+  }
+  if (ready && !user) {
+    return <Navigate to="/login" />;
+  }
+
+  let { subpage } = useParams();
+
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  if (redirect) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+        <Link
+          className={` py-2 px-6 ${
+            subpage === "profile" && "bg-primary text-white rounded-full"
+          }`}
+          to="/account"
+        >
+          My Profile
+        </Link>
+        <Link
+          className={` py-2 px-6 ${
+            subpage === "bookings" && "bg-primary text-white rounded-full"
+          }`}
+          to="/account/bookings"
+        >
+          My Bookings
+        </Link>
+        <Link
+          className={` py-2 px-6 ${
+            subpage === "places" && "bg-primary text-white rounded-full"
+          }`}
+          to="/account/places"
+        >
+          My Places
+        </Link>
+      </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email})
+          <Button
+            gradientDuoTone="pinkToOrange"
+            className="mx-auto w-full max-w-sm mt-5"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
