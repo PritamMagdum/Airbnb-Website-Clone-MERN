9f312d0e7fdd2d15622656224405b88fa2b27935
Pritam <pritammagdum24@gmail.com>
2024-03-25 17:46:40 +0530

    Added Functionality to Upload Photo By Link

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,9 +1,35 @@
 import { Button, Checkbox, Label, TextInput, Textarea } from "flowbite-react";
 import { Link, useParams } from "react-router-dom";
+import Perks from "./Perks";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Places() {
   const { action } = useParams();
-  console.log(action);
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [photoLink, setPhotoLink] = useState("");
+  const [description, setDescription] = useState("");
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  console.log({ photoLink });
+  // console.log(action);
+  async function addPhotoByLink(e) {
+    e.preventDefault();
+    const { data: filename } = await axios.post("/upload-by-link", {
+      link: photoLink,
+    });
+    setAddedPhotos((prev) => {
+      return [...prev, filename];
+    });
+    setPhotoLink("");
+  }
+
   return (
     <div className="">
       {action !== "new" && (
@@ -36,12 +62,22 @@ export default function Places() {
         <div className="">
           <form className="flex flex-col gap-2 px-5">
             <Label className="text-xl">Title</Label>
-            <TextInput type="text" placeholder="example - My favorite place" />
+            <TextInput
+              type="text"
+              placeholder="example - My favorite place"
+              // value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
             <span className="text-sm text-gray-600 font-medium">
               Title for your place, should be short and catchy for advertisement
             </span>
             <Label className="text-xl">Address</Label>
-            <TextInput type="text" placeholder="address" />
+            <TextInput
+              type="text"
+              placeholder="address"
+              // value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
             <span className="text-sm text-gray-600 font-medium">
               Address to this place
             </span>
@@ -53,11 +89,34 @@ export default function Places() {
               <TextInput
                 placeholder="add photo using link.....jpg"
                 className="w-full"
+                value={photoLink}
+                onChange={(e) => setPhotoLink(e.target.value)}
               />
-              <Button className="min-w-28">Add Photo</Button>
+              <Button onClick={addPhotoByLink} className="min-w-28">
+                Add Photo
+              </Button>
             </div>
-            <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              <Button color="transparent" className="border p-6 text-xl">
+
+            <div className="grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+              {addedPhotos.length > 0 &&
+                addedPhotos.map((link) => (
+                  <div>
+                    <img
+                      className="rounded-2xl"
+                      src={`http://localhost:8080/uploads/${link}`}
+                      alt={link}
+                    />
+                  </div>
+                ))}
+              <label
+                color="transparent"
+                className="flex items-center rounded-2xl border justify-center cursor-pointer text-xl"
+              >
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={handleUploadPhoto}
+                />
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -73,138 +132,33 @@ export default function Places() {
                   />
                 </svg>
                 Upload
-              </Button>
+              </label>
             </div>
             <Label className="text-xl">Description</Label>
             <span className="text-sm text-gray-600 font-medium">
               description of the place
             </span>
-            <Textarea rows={5} />
+            <Textarea
+              // value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              rows={5}
+            />
             <Label className="text-xl">Perks</Label>
             <span className="text-sm text-gray-600 font-medium">
               select all the perks of your place
             </span>
             <div className="grid gap-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-2">
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.288 15.038a5.25 5.25 0 0 1 7.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 0 1 1.06 0Z"
-                  />
-                </svg>
-
-                <span>WIFI</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-                  />
-                </svg>
-
-                <span>Free Parking</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
-                  />
-                </svg>
-
-                <span>TV</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-                  />
-                </svg>
-
-                <span>Private entrance</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
-                  />
-                </svg>
-
-                <span>Free Breakfast</span>
-              </label>
-              <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
-                <Checkbox />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m3.75 7.5 16.5-4.125M12 6.75c-2.708 0-5.363.224-7.948.655C2.999 7.58 2.25 8.507 2.25 9.574v9.176A2.25 2.25 0 0 0 4.5 21h15a2.25 2.25 0 0 0 2.25-2.25V9.574c0-1.067-.75-1.994-1.802-2.169A48.329 48.329 0 0 0 12 6.75Zm-1.683 6.443-.005.005-.006-.005.006-.005.005.005Zm-.005 2.127-.005-.006.005-.005.005.005-.005.005Zm-2.116-.006-.005.006-.006-.006.005-.005.006.005Zm-.005-2.116-.006-.005.006-.005.005.005-.005.005ZM9.255 10.5v.008h-.008V10.5h.008Zm3.249 1.88-.007.004-.003-.007.006-.003.004.006Zm-1.38 5.126-.003-.006.006-.004.004.007-.006.003Zm.007-6.501-.003.006-.007-.003.004-.007.006.004Zm1.37 5.129-.007-.004.004-.006.006.003-.004.007Zm.504-1.877h-.008v-.007h.008v.007ZM9.255 18v.008h-.008V18h.008Zm-3.246-1.87-.007.004L6 16.127l.006-.003.004.006Zm1.366-5.119-.004-.006.006-.004.004.007-.006.003ZM7.38 17.5l-.003.006-.007-.003.004-.007.006.004Zm-1.376-5.116L6 12.38l.003-.007.007.004-.004.007Zm-.5 1.873h-.008v-.007h.008v.007ZM17.25 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Zm0 4.5a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
-                  />
-                </svg>
-
-                <span>Radio</span>
-              </label>
+              <Perks selected={perks} onChange={setPerks} />
             </div>
             <Label className="text-xl">Extra Information</Label>
             <span className="text-sm text-gray-600 font-medium">
               house rules, advance booking, etc
             </span>
-            <Textarea rows={5} />
+            <Textarea
+              rows={5}
+              // value={extraInfo}
+              onChange={(e) => setExtraInfo(e.target.value)}
+            />
             <Label className="text-xl">Check In & Out times</Label>
             <span className="text-sm text-gray-600 font-medium">
               add check in and out times, remember to have some time window for
@@ -213,18 +167,31 @@ export default function Places() {
             <div className="grid sm:grid-cols-3 gap-2">
               <div className="">
                 <Label className="text-sm">Check in time</Label>
-                <TextInput placeholder="12:00" />
+                <TextInput
+                  placeholder="12:00"
+                  // value={checkIn}
+                  onChange={(e) => setCheckIn(e.target.value)}
+                />
               </div>
               <div className="">
                 <Label className="text-sm">Check out time</Label>
-                <TextInput />
+                <TextInput
+                  // value={checkOut}
+                  onChange={(e) => setCheckOut(e.target.value)}
+                />
               </div>
               <div className="">
                 <Label className="text-sm">Number of guests</Label>
-                <TextInput />
+                <TextInput
+                  type="number"
+                  // value={maxGuests}
+                  onChange={(e) => setMaxGuests(e.target.value)}
+                />
               </div>
             </div>
-            <Button gradientDuoTone="pinkToOrange">Save</Button>
+            <Button gradientDuoTone="pinkToOrange" className="my-4">
+              Save
+            </Button>
           </form>
         </div>
       )}
