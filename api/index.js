927c2971481fc7a4d567188dfaa7002455942153
Pritam <pritammagdum24@gmail.com>
2024-03-26 10:12:21 +0530

    Added Endpoint or API to Get the All Places from Database

--- api/index.js
+++ api/index.js
@@ -161,4 +161,12 @@ app.post("/places", (req, res) => {
     res.json(placeDoc);
   });
 });
+
+app.get("/places", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, process.env.JWT_SECRET_KEY, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
 app.listen(8080);
